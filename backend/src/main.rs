4e059a67728f01d26ec0b22b2b30a9bdca0a373a
legendary-cookie <github.com@xolley.de>
2021-10-17 16:44:59 +0200

    /api/users

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,33 @@
+use std::collections::HashMap;
+
 use backlib::establish_connection;
 use backlib::models::User;
 use backlib::schema::users::dsl::*;
-use diesel::{RunQueryDsl};
+use diesel::RunQueryDsl;
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    let get_users = warp::get()
+        .and(warp::path("api"))
+        .and(warp::path("users"))
+        .and(warp::path::end())
+        .and_then(get_users);
+
+    warp::serve(get_users).run(([127, 0, 0, 1], 3030)).await;
+}
+
+async fn get_users() -> Result<impl warp::Reply, warp::Rejection> {
+    let mut result = HashMap::new();
+
     let connection = establish_connection();
     let results = users
         .load::<User>(&connection)
         .expect("Error loading posts");
 
-    println!("Displaying {} users", results.len());
     for user in results {
-        println!("{}:{}:{}", user.email, user.username, user.pwhash);
+        result.insert(user.email, user.username);
     }
+
+    Ok(warp::reply::json(&result))
 }
