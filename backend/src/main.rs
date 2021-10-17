139967d154d12f4e244a356715463b06d558e405
legendary-cookie <github.com@xolley.de>
2021-10-17 18:25:36 +0200

    added websocket for /chat

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,6 +10,7 @@ use backlib::WebResult;
 use diesel::ExpressionMethods;
 use diesel::QueryDsl;
 use diesel::RunQueryDsl;
+use futures_util::{FutureExt, StreamExt};
 use sha256::digest;
 use warp::reject;
 use warp::reply;
@@ -35,9 +36,29 @@ async fn main() {
         .and(warp::body::json())
         .and_then(login_handler);
 
+    // Test route, get your user ID of your JWT token
     let user_route = warp::path!("user").and(with_auth()).and_then(user_handler);
 
-    let routes = login_route.or(user_route).recover(error::handle_rejection);
+    let echo_route = warp::path("echo")
+        // The `ws()` filter will prepare the Websocket handshake.
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            // And then our closure will be called when it completes...
+            ws.on_upgrade(|websocket| {
+                // Just echo all messages back...
+                let (tx, rx) = websocket.split();
+                rx.forward(tx).map(|result| {
+                    if let Err(e) = result {
+                        eprintln!("websocket error: {:?}", e);
+                    }
+                })
+            })
+        });
+
+    let routes = login_route
+        .or(echo_route)
+        .or(user_route)
+        .recover(error::handle_rejection);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
